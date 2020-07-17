ea7ef95a0a5574db1072f4b9f47fd9bb230dc15f
Anushka Verma <v.anushka786@gmail.com>
2020-07-17 19:24:55 +0530

    add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Navbar } from "react-bootstrap";
 
 const Header = () => {
-    return (
-        <div>Header</div>
-    );
-}
+  return (
+    <Navbar
+      bg="dark"
+      variant="dark"
+      className="text-center p-4"
+      style={{ color: "#fff" }}
+    >
+      <h1 style={{ textTransform: "uppercase", marginLeft: "38%" }}>
+        Meme Generator
+      </h1>
+    </Navbar>
+  );
+};
 
 export default Header;
